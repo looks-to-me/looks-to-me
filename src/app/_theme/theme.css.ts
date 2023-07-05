68ef75173cad3a3e1975488aaa6da26835a6c366
Karibash <y.karibayashi@gmail.com>
2023-07-05 20:03:00 +0900

    [feature] Add a typography styles

--- src/app/_theme/theme.css.ts
+++ src/app/_theme/theme.css.ts
@@ -25,4 +25,18 @@ export const theme = createGlobalTheme(':root', {
       borderMuted: '',
     },
   },
+  font: {
+    size: {
+      tiny: '.75rem',
+      normal: '.875rem',
+      medium: '1rem',
+      large: '1.25rem',
+    },
+    weight: {
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semiBold: '600',
+    },
+  },
 });
--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -1,8 +1,41 @@
-import { style } from '@vanilla-extract/css';
+import { style, globalStyle } from '@vanilla-extract/css';
+import { calc } from '@vanilla-extract/css-utils';
 
 import { theme } from './_theme';
 
 export const body = style({
   color: theme.color.tokens.text,
   backgroundColor: theme.color.tokens.background,
+  fontSize: theme.font.size.normal,
+  fontWeight: theme.font.weight.normal,
+});
+
+globalStyle('h1', {
+  fontSize: calc.add(theme.font.size.normal, '1.125rem'),
+  fontWeight: theme.font.weight.semiBold,
+});
+
+globalStyle('h2', {
+  fontSize: calc.add(theme.font.size.normal, '.625rem'),
+  fontWeight: theme.font.weight.semiBold,
+});
+
+globalStyle('h3', {
+  fontSize: calc.add(theme.font.size.normal, '.375rem'),
+  fontWeight: theme.font.weight.semiBold,
+});
+
+globalStyle('h4', {
+  fontSize: calc.add(theme.font.size.normal, '.125rem'),
+  fontWeight: theme.font.weight.semiBold,
+});
+
+globalStyle('h5', {
+  fontSize: theme.font.size.normal,
+  fontWeight: theme.font.weight.semiBold,
+});
+
+globalStyle('h6', {
+  fontSize: calc.subtract(theme.font.size.normal, '.125rem'),
+  fontWeight: theme.font.weight.semiBold,
 });
