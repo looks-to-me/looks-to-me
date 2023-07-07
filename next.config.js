0c6f5cef669920dab05105d184b9192f733eab5a
Karibash <y.karibayashi@gmail.com>
2023-07-07 20:29:38 +0900

    [chore] Make the Storybook published under Next.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  rewrites: async () => [
+    {
+      source: '/storybook/',
+      destination: '/storybook/index.htm',
+    },
+  ],
 };
 
 module.exports = withVanillaExtract(nextConfig);
