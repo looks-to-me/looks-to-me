29914fd906fcfcb116de377c76157608eab7f3ed
Karibash <y.karibayashi@gmail.com>
2023-07-21 11:25:58 +0900

    [refactor] Make environment variables be called at runtime

--- src/app/_libs/db/index.ts
+++ src/app/_libs/db/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from 'drizzle-orm/d1';
 
 import { schema } from './schema';
+import { memoize } from '../../_helpers/memoize';
 import { env } from '../env';
 
-export const db = drizzle(env.DB, { schema });
+export const db = memoize(() => {
+  return drizzle(env().DB, { schema });
+});
--- src/app/_libs/env/index.ts
+++ src/app/_libs/env/index.ts
@@ -2,13 +2,17 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { binding } from 'cf-bindings-proxy';
 import { z } from 'zod';
 
+import { memoize } from '../../_helpers/memoize';
+
 import type { D1Database } from '@cloudflare/workers-types';
 
-export const env = createEnv({
-  server: {
-    DB: z.custom<D1Database>(value => value && typeof value === 'object'),
-  },
-  runtimeEnv: {
-    DB: binding('DB'),
-  },
+export const env = memoize(() => {
+  return createEnv({
+    server: {
+      DB: z.custom<D1Database>(value => value && typeof value === 'object'),
+    },
+    runtimeEnv: {
+      DB: binding('DB'),
+    },
+  });
 });
