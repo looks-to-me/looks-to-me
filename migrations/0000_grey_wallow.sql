6b654716787dbe4ff68e8c87f4cf86a1036fa4fb
gn-t-k <gntk1995422@gmail.com>
2023-08-12 07:26:37 +0900

    マイグレーション履歴消した

--- migrations/0000_grey_wallow.sql
+++ migrations/0000_grey_wallow.sql
@@ -3,3 +3,10 @@ CREATE TABLE `images` (
 	`user_id` text NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`display_name` text NOT NULL,
+	`avatar_url` text NOT NULL
+);
--- migrations/0000_unique_lilith.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `users` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text
-);
