065a164555a816ec5514d3fbc983507e5c4be448
Karibash <y.karibayashi@gmail.com>
2023-08-12 16:19:13 +0900

    [feature] Add a aspect ratio component

---
+++ src/app/_components/aspect-ratio/aspect-ratio.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
+
+import type { ComponentPropsWithoutRef } from 'react';
+
+export type AspectRatioProps = ComponentPropsWithoutRef<typeof AspectRatioPrimitive.Root>;
+
+export const AspectRatio = AspectRatioPrimitive.Root;
