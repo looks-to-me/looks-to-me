0315744408e5372d47fbd7f617898a2e68506374
gn-t-k <gntk1995422@gmail.com>
2023-08-12 19:48:40 +0900

    スキーマ修正

--- src/app/_libs/db/schema/tables/users.ts
+++ src/app/_libs/db/schema/tables/users.ts
@@ -1,5 +1,5 @@
 import { relations } from 'drizzle-orm';
-import { text, sqliteTable, integer } from 'drizzle-orm/sqlite-core';
+import { text, sqliteTable } from 'drizzle-orm/sqlite-core';
 
 import { images } from './images';
 
@@ -10,8 +10,6 @@ export const users = sqliteTable('users', {
   name: text('name').notNull(),
   displayName: text('display_name').notNull(),
   avatarUrl: text('avatar_url').notNull(),
-  createdAt: integer('created_at', { mode: 'timestamp_ms'}).notNull(),
-  updatedAt: integer('updated_at', { mode: 'timestamp_ms'}).notNull(),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
