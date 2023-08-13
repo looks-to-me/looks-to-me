8655ed7b7e75577717735c743cd5f53d5b4a6588
gn-t-k <gntk1995422@gmail.com>
2023-08-13 15:31:03 +0900

    Revert "wip"
    This reverts commit 5136700eb089261f41561947c4f1bb7effd096ab.

--- src/app/(main)/_components/login-button/login-button.tsx
+++ src/app/(main)/_components/login-button/login-button.tsx
@@ -1,10 +1,16 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useCallback } from 'react';
+
 import * as styles from './login-button.css';
 import { Button } from '../../../_components/button';
 import { PrefersColorScheme } from '../../../_components/prefers-color-scheme';
 import GitHubBlack from '../../../_icons/github-black.svg';
 import GitHubWhite from '../../../_icons/github-white.svg';
+import { supabase } from '../../../_libs/auth/client/instance';
 
-import type { FC, ComponentPropsWithoutRef } from 'react';
+import type { MouseEventHandler, FC, ComponentPropsWithoutRef } from 'react';
 
 type OmitProps = 'variant' | 'borderless' | 'asChild';
 
@@ -16,16 +22,28 @@ export const LoginButton: FC<LoginButtonProps> = ({
   className,
   ...props
 }) => {
+  const router = useRouter();
+  const handleClick = useCallback<MouseEventHandler<HTMLButtonElement>>(async () => {
+    // TODO: エラーハンドリング
+    // const { data: _data, error: _error } =
+    await supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+
+    router.refresh();
+  }, [router]);
+
   return (
-    <form action="/auth/login" method='post'>
-      <Button {...props} className={className} variant="primary" >
-        <PrefersColorScheme
-          className={styles.icon}
-          light={<GitHubBlack />}
-          dark={<GitHubWhite />}
-        />
-        Login with GitHub
-      </Button>
-    </form>
+    <Button {...props} className={className} variant="primary" onClick={handleClick}>
+      <PrefersColorScheme
+        className={styles.icon}
+        light={<GitHubBlack />}
+        dark={<GitHubWhite />}
+      />
+      Login with GitHub
+    </Button>
   );
 };
