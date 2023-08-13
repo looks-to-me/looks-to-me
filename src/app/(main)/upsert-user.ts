9a7f68df3bdbc81f089f10d38bd69e939c06a573
gn-t-k <gntk1995422@gmail.com>
2023-08-13 16:26:39 +0900

    ユーザーをdbにupsert

---
+++ src/app/(main)/upsert-user.ts
@@ -0,0 +1,44 @@
+'use server';
+
+import { eq } from 'drizzle-orm';
+
+import { db } from '../_libs/db';
+import { users } from '../_libs/db/schema/tables/users';
+
+import type { AuthUser } from '../_libs/auth/type/auth-user';
+
+type UpsertUser = (authUser: AuthUser) => Promise<void>;
+export const upsertUser: UpsertUser = async (authUser) => {
+  const dbUser = await db().select().from(users).where(eq(users.id, authUser.id)).get();
+
+  if (!dbUser) {
+    await db()
+      .insert(users)
+      .values({
+        id: authUser.id,
+        name: authUser.accountName,
+        displayName: authUser.displayName,
+        avatarUrl: authUser.avatarUrl,
+        registeredAt: new Date(),
+      })
+      .run();
+
+    return;
+  }
+
+  if (
+    dbUser.name !== authUser.accountName ||
+    dbUser.displayName !== authUser.displayName ||
+    dbUser.avatarUrl !== authUser.avatarUrl 
+  ) {
+    await db()
+      .update(users)
+      .set({
+        name: authUser.accountName,
+        displayName: authUser.displayName,
+        avatarUrl: authUser.avatarUrl,
+      })
+      .where(eq(users.id, authUser.id))
+      .run();
+  }
+};
