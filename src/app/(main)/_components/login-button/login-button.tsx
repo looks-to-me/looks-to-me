7f8ca44fa0b3c759e09b87b332c917172e192783
gn-t-k <gntk1995422@gmail.com>
2023-08-13 13:44:13 +0900

    wip

--- src/app/(main)/_components/login-button/login-button.tsx
+++ src/app/(main)/_components/login-button/login-button.tsx
@@ -1,16 +1,10 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
-
 import * as styles from './login-button.css';
 import { Button } from '../../../_components/button';
 import { PrefersColorScheme } from '../../../_components/prefers-color-scheme';
 import GitHubBlack from '../../../_icons/github-black.svg';
 import GitHubWhite from '../../../_icons/github-white.svg';
-import { supabase } from '../../../_libs/auth/client/instance';
 
-import type { MouseEventHandler, FC, ComponentPropsWithoutRef } from 'react';
+import type { FC, ComponentPropsWithoutRef } from 'react';
 
 type OmitProps = 'variant' | 'borderless' | 'asChild';
 
@@ -22,28 +16,16 @@ export const LoginButton: FC<LoginButtonProps> = ({
   className,
   ...props
 }) => {
-  const router = useRouter();
-  const handleClick = useCallback<MouseEventHandler<HTMLButtonElement>>(async () => {
-    // TODO: エラーハンドリング
-    // const { data: _data, error: _error } =
-    await supabase.auth.signInWithOAuth({
-      provider: 'github',
-      options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
-      },
-    });
-
-    router.refresh();
-  }, [router]);
-
   return (
-    <Button {...props} className={className} variant="primary" onClick={handleClick}>
-      <PrefersColorScheme
-        className={styles.icon}
-        light={<GitHubBlack />}
-        dark={<GitHubWhite />}
-      />
-      Login with GitHub
-    </Button>
+    <form action="/auth/login" method='post'>
+      <Button {...props} className={className} variant="primary" >
+        <PrefersColorScheme
+          className={styles.icon}
+          light={<GitHubBlack />}
+          dark={<GitHubWhite />}
+        />
+        Login with GitHub
+      </Button>
+    </form>
   );
 };
