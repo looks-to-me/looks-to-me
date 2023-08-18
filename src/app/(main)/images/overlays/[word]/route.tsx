9a0730bece6fc0e6b0499e2c4b0db4f0a8886bea
Kyosuke Awata <kyou0209@gmail.com>
2023-08-18 21:23:02 +0900

    [fix] small fix

--- src/app/(main)/images/overlays/[word]/route.tsx
+++ src/app/(main)/images/overlays/[word]/route.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from 'next/server';
+import * as z from 'zod';
 
 import type { NextRequest } from 'next/server';
 
@@ -6,63 +7,40 @@ export const runtime = 'edge';
 
 export const contentType = 'image/png';
 
-const isTooLong = (word: string) => word.length > 32;
+const colorSchema = z.union([z.literal('black'), z.literal('white')]).default('black');
+const wordSchema = z
+  .string()
+  .regex(/^[a-zA-Z]+$/, { message: 'Must be a alphabetic.' })
+  .max(32, { message: 'Must be less than 32 characters.' })
+  .transform((word) => `${word[0]?.toUpperCase()}${word.slice(1).toLowerCase()}`);
 
-const isAlphabetic = (word: string) => /^[a-zA-Z]+$/.test(word);
+export const GET = (request: NextRequest, { params }: { params: { word: string } }) => {
+  const word = wordSchema.safeParse(params.word);
+  if (!word.success) {
+    return new Response(word.error.message, { status: 400 });
+  }
 
-const normalize = (word: string) => {
-  return `${word[0]?.toUpperCase()}${word.slice(1).toLowerCase()}`;
-};
-
-const getColorQuery = (request: NextRequest) => {
   const { searchParams } = new URL(request.url);
-  const color = searchParams.get('color');
-  if (!color) {
-    // default color
-    return 'black';
-  }
-  if (color === 'white' || color === 'black') {
-    return color;
-  }
-  return null;
-};
-
-export const GET = (
-  request: NextRequest,
-  { params }: { params: { word: string } },
-) => {
-  if (isTooLong(params.word)) {
-    return new Response('word must be less than 32 characters', {
-      status: 400,
-    });
-  }
-  if (!isAlphabetic(params.word)) {
-    return new Response('word must be alphabetic', { status: 400 });
-  }
-  const word = normalize(params.word);
-
-  const color = getColorQuery(request);
-  if (!color) {
+  const color = colorSchema.safeParse(searchParams.get('color') ?? undefined);
+  if (!color.success) {
     return new Response('color must be white or black', { status: 400 });
   }
 
-  return new ImageResponse((
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      <div style={{ display: 'flex', fontSize: 200, color }}>
-        L{word.at(0)}TM
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <div style={{ display: 'flex', fontSize: 200, color: color.data }}>L{word.data.at(0)}TM</div>
+        <div style={{ display: 'flex', fontSize: 50, color: color.data }}>Looks {word.data} To Me</div>
       </div>
-      <div style={{ display: 'flex', fontSize: 50, color }}>
-        Looks {word} To Me
-      </div>
-    </div>
-  ));
+    ),
+  );
 };
