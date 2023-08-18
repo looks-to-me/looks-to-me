9d87eaf4f097dbcaab48859961419dff8449d5a5
zack-msyc86 <masaya.okazaki.zack@gmail.com>
2023-08-18 23:12:43 +0900

    r2のBucket追加

--- src/app/_libs/env/index.ts
+++ src/app/_libs/env/index.ts
@@ -4,15 +4,17 @@ import { z } from 'zod';
 
 import { memoize } from '../../_helpers/memoize';
 
-import type { D1Database } from '@cloudflare/workers-types';
+import type { D1Database, R2Bucket } from '@cloudflare/workers-types';
 
 export const env = memoize(() => {
   return createEnv({
     server: {
       DB: z.custom<D1Database>(value => value && typeof value === 'object'),
+      BUCKET: z.custom<R2Bucket>(value => value && typeof value === 'object'),
     },
     runtimeEnv: {
       DB: binding('DB'),
+      BUCKET: binding('BUCKET'),
     },
   });
 });
