e0f96e665d23aaf8ed683e79d0842bf30d91aadb
Karibash <y.karibayashi@gmail.com>
2023-08-18 10:34:21 +0900

    [feature] Move the home page component under the home root group

---
+++ src/app/(main)/(home)/page.tsx
@@ -0,0 +1,15 @@
+import { Header } from '../_components/header';
+
+import type { FC } from 'react';
+
+export const runtime = 'edge';
+
+const HomePage: FC = () => {
+  return (
+    <Header>
+      Home
+    </Header>
+  );
+};
+
+export default HomePage;
