c1abba8712585890d323b6765ea733dc1b809749
gn-t-k <gntk1995422@gmail.com>
2023-08-18 16:01:01 +0900

    actionの場所移動した

--- src/app/(main)/_components/sync-o-auth-user/upsert-user.ts
+++ src/app/(main)/_components/sync-o-auth-user/upsert-user.ts
@@ -1,9 +1,9 @@
 'use server';
 
-import { db } from './_libs/db';
-import { users } from './_libs/db/schema/tables/users';
+import { db } from '../../../_libs/db';
+import { users } from '../../../_libs/db/schema/tables/users';
 
-import type { AuthUser } from './_libs/auth/type/auth-user';
+import type { AuthUser } from '../../../_libs/auth/type/auth-user';
 
 type UpsertUser = (authUser: AuthUser) => Promise<void>;
 export const upsertUser: UpsertUser = async (authUser) => {
