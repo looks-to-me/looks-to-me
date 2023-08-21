a85fa550e77bcb9d014e70c8076386b88acfe52a
Karibash <y.karibayashi@gmail.com>
2023-08-21 20:33:29 +0900

    [bugfix] Fix forgot to write migration of users table

--- migrations/0003_tired_onslaught.sql
+++ migrations/0003_tired_onslaught.sql
@@ -1,8 +1,7 @@
-/*
- SQLite does not support "Drop not null from column" out of the box, we do not generate automatic migration for that, so it has to be done manually
- Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
-                  https://www.sqlite.org/lang_altertable.html
-                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
-
- Due to that we don't generate migration automatically and it has to be done manually
-*/
+ALTER TABLE `users` RENAME `display_name` TO `_display_name`;
+--> statement-breakpoint
+ALTER TABLE `users` ADD `display_name` text;
+--> statement-breakpoint
+UPDATE `users` SET `display_name` = `_display_name`;
+--> statement-breakpoint
+ALTER TABLE `users` DROP `_display_name`;
