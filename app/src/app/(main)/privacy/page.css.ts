8fdee0aa9c5f176f188aab3d54239264e286c1c3
Karibash <y.karibayashi@gmail.com>
2023-08-25 17:34:22 +0900

    [feature] Add a privacy policy page

---
+++ app/src/app/(main)/privacy/page.css.ts
@@ -0,0 +1,33 @@
+import { style } from '@vanilla-extract/css';
+
+import { theme } from '../../_theme';
+
+export const title = style({
+  fontWeight: 600,
+});
+
+export const main = style({
+  padding: '16px',
+});
+
+export const article = style({
+  maxWidth: '800px',
+  padding: '16px',
+  margin: '0 auto',
+});
+
+export const paragraph = style({
+  lineHeight: 1.5,
+  margin: 0,
+});
+
+export const sectionTitle = style({
+  marginTop: '32px',
+  paddingBottom: '8px',
+  borderBottom: `solid 1px ${theme.color.token.semantic.border}`,
+  fontSize: theme.size.font.large,
+});
+
+export const listItem = style({
+  margin: '8px 0',
+});
