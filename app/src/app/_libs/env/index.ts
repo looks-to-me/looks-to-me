7c34950ceee092c9e8ce3ff2d9df81a73b11460c
Karibash <y.karibayashi@gmail.com>
2023-08-26 22:12:48 +0900

    [feature] Add an endpoint to retrieve uploaded images

--- app/src/app/_libs/env/index.ts
+++ app/src/app/_libs/env/index.ts
@@ -9,12 +9,16 @@ import type { D1Database, R2Bucket } from '@cloudflare/workers-types';
 export const env = memoize(() => {
   return createEnv({
     server: {
+      NODE_ENV: z.enum(['production', 'development', 'test']),
       DB: z.custom<D1Database>(value => value && typeof value === 'object'),
       BUCKET: z.custom<R2Bucket>(value => value && typeof value === 'object'),
+      POST_RAW_IMAGE_TOKEN: z.string(),
     },
     runtimeEnv: {
+      NODE_ENV: process.env.NODE_ENV,
       DB: binding('DB'),
       BUCKET: binding('BUCKET'),
+      POST_RAW_IMAGE_TOKEN: process.env['POST_RAW_IMAGE_TOKEN'],
     },
   });
 });
