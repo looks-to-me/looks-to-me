4902e11e56cde6f4fe44773b9c57725e1349216d
Karibash <y.karibayashi@gmail.com>
2023-08-26 21:49:52 +0900

    [refactor] Improve the process of saving posts

---
+++ app/src/app/(main)/posts/new/_components/post-create-form/actions/submitPost.ts
@@ -0,0 +1,77 @@
+'use server';
+
+import { createId } from '@paralleldrive/cuid2';
+import { and, eq } from 'drizzle-orm';
+import { z } from 'zod';
+
+import { getUserMetadata } from '../../../../../../_libs/auth/server/get-user-metadata';
+import { db } from '../../../../../../_libs/db';
+import { schema } from '../../../../../../_libs/db/schema';
+import { storage } from '../../../../../../_libs/storage';
+
+class SubmitPostError extends Error {
+  public override readonly name = 'SubmitPostError';
+
+  constructor(message: string, options?: ErrorOptions) {
+    super(message, options);
+  }
+}
+
+const inputsSchema = z.object({
+  image: z.custom<Blob>(value => value instanceof Blob),
+  word: z.string()
+    .regex(/^[a-zA-Z]+$/, { message: 'Must be a alphabetic.' })
+    .max(32, { message: 'Must be less than 32 characters.' })
+    .transform((word) => `${word[0]?.toUpperCase()}${word.slice(1).toLowerCase()}`),
+});
+
+export const submitPost = async (formData: FormData): Promise<void> => {
+  const inputs = inputsSchema.parse({
+    image: formData.get('image'),
+    word: formData.get('word'),
+  });
+
+  const userMetadata = await getUserMetadata();
+  if (!userMetadata) throw new SubmitPostError('Unauthorized');
+
+  const userProvider = await db()
+    .select()
+    .from(schema.userProviders)
+    .where(
+      and(
+        eq(schema.userProviders.provider, userMetadata.provider),
+        eq(schema.userProviders.sub, userMetadata.sub),
+      ),
+    )
+    .get();
+  if (!userProvider) throw new SubmitPostError('Unauthorized');
+
+  const imageId = createId();
+  await storage().put(`users/${userProvider.userId}/images/${imageId}`, await inputs.image.arrayBuffer());
+
+  const postId = createId();
+
+  // TODO: Make use of transaction or batch.
+  // @see: https://github.com/drizzle-team/drizzle-orm/issues/758
+  {
+    await db()
+      .insert(schema.images)
+      .values({
+        id: imageId,
+        userId: userProvider.userId,
+        uploadedAt: new Date(),
+      })
+      .run();
+
+    await db()
+      .insert(schema.posts)
+      .values({
+        id: postId,
+        userId: userProvider.userId,
+        imageId: imageId,
+        word: inputs.word,
+        postedAt: new Date(),
+      })
+      .run();
+  }
+};
--- app/src/app/_libs/storage/index.ts
+++ app/src/app/_libs/storage/index.ts
@@ -1,27 +1,4 @@
-import { createId } from '@paralleldrive/cuid2';
+import { env } from '../env';
 
-import type { R2Bucket, R2Object } from '@cloudflare/workers-types';
-
-class UploadFileError extends Error {
-  public override readonly name = 'UploadFileError';
-  public static readonly message = 'Failed to upload file';
-
-  constructor(options?: ErrorOptions) {
-    super(UploadFileError.message, options);
-  }
-}
-
-export const uploadFile: UploadFile = (client) => async (file) => {
-  // upload image to R2 and returns the image key
-  try {
-    const buf = await file.arrayBuffer();
-    const result = await client.put(createId(), buf);
-    if (result) return result;
-  } catch (error) {
-    throw new UploadFileError({ cause: error });
-  }
-
-  throw new UploadFileError();
-};
-
-export type UploadFile = (client: R2Bucket) => (file: File) => Promise<R2Object>;
+// TODO: Make it mock when it runs in Storybook.
+export const storage = () => env().BUCKET;
