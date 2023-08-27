4ebc1b594e5cef99fa5a931227ff93439682abd6
zack-msyc86 <masaya.okazaki.zack@gmail.com>
2023-08-28 00:49:57 +0900

    fix overflow when selecting large images.

--- app/src/app/(main)/posts/new/_components/input-image-with-preview/input-image-with-preview.css.ts
+++ app/src/app/(main)/posts/new/_components/input-image-with-preview/input-image-with-preview.css.ts
@@ -1,14 +1,21 @@
 import { style } from '@vanilla-extract/css';
 
 export const wrapper = style({
+  maxWidth: '600px',
 });
 
 export const dropZone = style({
   border: 'solid 1px white',
   padding: '20px',
   textAlign: 'center',
-  height: '200px',
+  minHeight: '200px',
+  maxHeight: '80%',
   borderRadius: '4px',
   cursor: 'pointer',
   transition: 'background-color 0.3s, border-color 0.3s',
+});
+
+export const image = style({
+  maxWidth: '90%',
+  maxHeight: '90%',
 });
