036aef35f61879451ab459386dadb72188eafcfe
Karibash <y.karibayashi@gmail.com>
2023-08-29 23:48:59 +0900

    [feature] Fix references to avatar images

--- app/src/app/(main)/_components/avatar-menu/avatar-menu.tsx
+++ app/src/app/(main)/_components/avatar-menu/avatar-menu.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -5,23 +6,25 @@ import * as styles from './avatar-menu.css';
 import { Popover, PopoverContent, PopoverTrigger } from '../../../_components/popover';
 import { LogoutButton } from '../logout-button';
 
-import type { UserMetadata } from '../../../_libs/auth/type/user-metadata';
+import type { UserEntity } from '../../_repositories/user-repository';
 import type { FC } from 'react';
 
 export type AvatarMenuProps = {
-  userMetadata: UserMetadata;
+  className?: string;
+  user: UserEntity;
 };
 
 export const AvatarMenu: FC<AvatarMenuProps> = ({
-  userMetadata,
+  className,
+  user,
 }) => {
   return (
     <Popover>
       <PopoverTrigger>
         <Image
-          className={styles.avatar}
-          src={userMetadata.avatar_url}
-          alt={userMetadata.name ?? userMetadata.user_name}
+          className={clsx(className, styles.avatar)}
+          src={`/images/avatars/${user.id}`}
+          alt={user.profile.displayName ?? user.profile.name}
           width={32}
           height={32}
         />
@@ -29,11 +32,11 @@ export const AvatarMenu: FC<AvatarMenuProps> = ({
       <PopoverContent>
         <div className={styles.content}>
           <div className={styles.accountInfoArea}>
-            <p className={styles.accountName}>{userMetadata.user_name}</p>
-            {userMetadata.name && (
-              <p className={styles.displayName}>{userMetadata.name}</p>
+            <p className={styles.accountName}>{user.profile.name}</p>
+            {user.profile.displayName && (
+              <p className={styles.displayName}>{user.profile.displayName}</p>
             )}
-            <Link href={`/@${userMetadata.user_name}/`}>Your profile</Link>
+            <Link href={`/@${user.profile.name}/`}>Your profile</Link>
           </div>
           <div className={styles.buttonArea}>
             <LogoutButton size="medium" />
