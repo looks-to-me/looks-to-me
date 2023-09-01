18688a20dbdbb2ffa3b497558b602b7f8e851197
sushidesu <hugemerge@gmail.com>
2023-09-01 20:04:41 +0900

    feature: Show user posts

--- app/src/app/(main)/[atUserName]/(profile)/@posts/page.tsx
+++ app/src/app/(main)/[atUserName]/(profile)/@posts/page.tsx
@@ -1,3 +1,11 @@
+import { notFound } from 'next/navigation';
+
+import { fetchUserPosts } from './_actions/fetch-user-posts';
+import { PostList } from '../../../_components/post-list';
+import { findUserByName } from '../../../_repositories/user-repository';
+import { getUserName } from '../../_helpers/getUserName';
+
+import type { InfiniteScrollFetcher } from '../../../../_components/infinite-scroll';
 import type { PageProps } from '../../../../_types/page-props';
 import type { UserProfilePageProps } from '../page';
 import type { FC } from 'react';
@@ -13,15 +21,30 @@ export type UserProfileMainPageProps = UserProfilePageProps & PageProps<{
   };
 }>;
 
-const UserProfileMainPage: FC<UserProfileMainPageProps> = async ({
+const UserProfilePostsPage: FC<UserProfileMainPageProps> = async ({
   params,
 }) => {
+  const userName = getUserName(params.atUserName);
+  if (!userName) return notFound();
+
+  const user = await findUserByName(userName);
+  if (!user) return notFound();
+
+  const posts = await fetchUserPosts(user.id);
+
+  const fetcher: InfiniteScrollFetcher = async args => {
+    'use server';
+    return await fetchUserPosts(user.id, args.cursor);
+  };
+
   return (
     <div>
-      <p>main</p>
-      <p>{params.atUserName}</p>
+      <PostList
+        posts={posts}
+        fetcher={fetcher}
+      />
     </div>
   );
 };
 
-export default UserProfileMainPage;
+export default UserProfilePostsPage;
