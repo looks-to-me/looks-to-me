d0e1daea9df78c92b2921021d58c2c6d317f433b
Karibash <y.karibayashi@gmail.com>
2023-09-01 16:56:28 +0900

    [feature] Add a logout page

---
+++ app/src/app/(main)/logout/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useMount } from 'ahooks';
+import { useRouter } from 'next/navigation';
+
+import { dispatch } from '../../_helpers/dispatch';
+import { supabase } from '../../_libs/auth/client/instance';
+
+import type { PageProps } from '../../_types/page-props';
+import type { LoginPageProps } from '../login/page';
+import type { FC } from 'react';
+
+export const runtime = 'edge';
+
+export type LogoutPageProps = PageProps<{
+  params: {
+    // empty
+  };
+  searchParams: {
+    // empty
+  };
+}>;
+
+const LogoutPage: FC<LoginPageProps> = () => {
+  const router = useRouter();
+
+  useMount(dispatch(async () => {
+    const { error } = await supabase.auth.signOut();
+    // TODO: error handling. show toast?
+    if (error) console.error(error);
+
+    router.back();
+    router.refresh();
+  }));
+
+  return null;
+};
+
+export default LogoutPage;
