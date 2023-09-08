5ae93e1baeef12a0d4cee6fb2890c45675e2bc0a
Karibash <y.karibayashi@gmail.com>
2023-09-08 20:36:30 +0900

    [feature] Add tags and post_tags tables

--- app/src/app/_libs/db/schema/index.ts
+++ app/src/app/_libs/db/schema/index.ts
@@ -1,18 +1,24 @@
 import { imagesRelations, images } from './tables/images';
+import { postTags, postTagsRelations } from './tables/post-tags';
 import { posts, postsRelations } from './tables/posts';
+import { tags, tagsRelations } from './tables/tags';
 import { userProfiles, userProfilesRelations } from './tables/user-profiles';
 import { userProviders, userProvidersRelations } from './tables/user-providers';
 import { users, usersRelations } from './tables/users';
 
 export const schema = {
-  userProfilesRelations,
+  images,
+  imagesRelations,
+  postTags,
+  postTagsRelations,
+  posts,
+  postsRelations,
+  tags,
+  tagsRelations,
   userProfiles,
+  userProfilesRelations,
   userProviders,
   userProvidersRelations,
   users,
   usersRelations,
-  posts,
-  postsRelations,
-  images,
-  imagesRelations,
 };
