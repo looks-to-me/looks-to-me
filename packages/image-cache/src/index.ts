cc4188c69162f10b05e262045ab8dbac283e99ed
Karibash <y.karibayashi@gmail.com>
2023-11-10 12:58:39 +0900

    [chore] Add test codes for package-image-cache

--- packages/image-cache/src/index.ts
+++ packages/image-cache/src/index.ts
@@ -1,41 +1,6 @@
-import type {
-  CacheStorage as WorkerCacheStorage,
-  Request as WorkerRequest,
-  Response as WorkerResponse,
-} from '@cloudflare/workers-types';
+import { fetchR2Cache, getCacheKey, getCaches, getR2CacheKey } from './helper';
 
-const getCaches = (): WorkerCacheStorage | undefined => {
-  if (typeof caches === 'undefined') return undefined;
-  return caches as unknown as WorkerCacheStorage;
-};
-
-const getCacheKey = (request: Request): WorkerRequest => {
-  return new Request(request.url.toString(), request) as unknown as WorkerRequest;
-};
-
-type R2CacheKeyParameters = {
-  path: string;
-  format?: 'webp' | 'png' | undefined;
-  width?: number | undefined;
-};
-
-const getR2CacheKey = (parameters: R2CacheKeyParameters): string => {
-  const path = parameters.path.replaceAll(/^\/|\/$/g, '');
-  const key = `caches/${path}/${parameters.format ?? 'unknown'}`;
-  if (parameters.width) return `${key}/${parameters.width}`;
-  return key;
-};
-
-const fetchR2Cache = async (bucket: R2Bucket, key: string): Promise<WorkerResponse | undefined> => {
-  const r2ObjectBody = await bucket.get(key);
-  if (!r2ObjectBody) return undefined;
-
-  const headers = new Headers();
-  r2ObjectBody.writeHttpMetadata(headers);
-  headers.set('etag', r2ObjectBody.httpEtag);
-  headers.set('cache-control', 'public, max-age=31536000, immutable');
-  return new Response(await r2ObjectBody.arrayBuffer(), { headers }) as unknown as WorkerResponse;
-};
+import type { Response as WorkerResponse } from '@cloudflare/workers-types';
 
 export type ImageCacheParameters = {
   request: Request;
