a1c63968e03409aef4ff734b1d0402939aca338b
Sakiyama Shingo <yamaa.exception@gmail.com>
2023-12-05 18:30:09 +0900

    Remove unused CSS styles

--- apps/web/src/app/_components/global-confirm-modal/global-confirm-modal.css.ts
+++ apps/web/src/app/_components/global-confirm-modal/global-confirm-modal.css.ts
@@ -1,10 +1,5 @@
 import { style } from '@vanilla-extract/css';
 
-export const main = style({
-  maxWidth: '400px',
-  width:'100%',
-});
-
 export const buttonWrapper = style({
   display: 'flex',
   justifyContent: 'flex-end',
