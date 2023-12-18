0a8c6309d88976a04b02caf1798856d73b0500a1
Sakiyama Shingo <yamaa.exception@gmail.com>
2023-12-18 21:59:59 +0900

    Add mute user test file

--- apps/web/src/app/(main)/_components/post-menu/actions/mute-user.ts
+++ apps/web/src/app/(main)/_components/post-menu/actions/mute-user.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { getLoginUser } from '../../../../../queries/user/get-login-user';
-import { findMuteUserByUserIdAndMuteUserId, saveMuteUser } from '../../../../../repositories/mute-user-repository';
+import { saveMuteUser } from '../../../../../repositories/mute-user-repository';
 
 export type MuteUserResult =
   | {
@@ -26,10 +26,6 @@ export const muteUserAction = async ({
 
   const isMe = muteUserId === user.id;
   if (isMe) return { type: 'error', reason: 'badRequest', message: 'Cannot mute yourself!' };
-
-  const muteUser = await findMuteUserByUserIdAndMuteUserId(user.id, muteUserId);
-  const isMuted = !!muteUser;
-  if (isMuted) return { type: 'error', reason: 'badRequest', message: 'Already muted!' };
 
   await saveMuteUser({
     userId: user.id,
