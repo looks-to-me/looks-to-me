2c9c42a0e1070fa0682151e96c00b624f318a23d
Sakiyama Shingo <yamaa.exception@gmail.com>
2023-12-25 19:29:53 +0900

    Remove demo-mute-users file and update page.tsx

--- apps/web/src/app/(main)/settings/(home)/@main/page.tsx
+++ apps/web/src/app/(main)/settings/(home)/@main/page.tsx
@@ -1,7 +1,7 @@
-import { demoMuteUsers } from './demo-mute-users';
 import * as styles from './page.css';
 import { MuteUserList } from '../../../../../components/domains/mute-user/mute-user-list';
 
+import type { User } from '../../../../../repositories/user-repository';
 import type { PageProps } from '../../../../../types/page-props';
 import type { SettingsHomePageProps } from '../page';
 import type { FC } from 'react';
@@ -16,6 +16,36 @@ export type SettingsHomeMainPageProps = SettingsHomePageProps & PageProps<{
     // empty
   };
 }>;
+
+const demoMuteUsers: User[] = [
+  {
+    id: '1',
+    profile: {
+      avatarUrl:
+        'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+      displayName: '@drizzle',
+      name: 'Drizzle',
+    },
+  },
+  {
+    id: '2',
+    profile: {
+      avatarUrl:
+        'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+      displayName: '@drizzle',
+      name: 'Drizzle',
+    },
+  },
+  {
+    id: '3',
+    profile: {
+      avatarUrl:
+        'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+      displayName: '@drizzle',
+      name: 'Drizzle',
+    },
+  },
+];
 
 const SettingsHomeMainPage: FC<SettingsHomeMainPageProps> = () => {
   return (
