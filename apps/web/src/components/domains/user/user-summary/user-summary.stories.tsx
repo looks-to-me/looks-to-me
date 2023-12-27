e11008d0960d3ea95d9ac28b67f3502f94d068bd
Sakiyama Shingo <yamaa.exception@gmail.com>
2023-12-27 16:18:02 +0900

    Revert "Delete user-summary.stories.tsx"
    This reverts commit 121a3935d11c03da3da2f3b0f722c711548468ca.

---
+++ apps/web/src/components/domains/user/user-summary/user-summary.stories.tsx
@@ -0,0 +1,22 @@
+import { UserSummary } from './user-summary';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  component: UserSummary,
+} as Meta<typeof UserSummary>;
+
+type Story = StoryObj<typeof UserSummary>;
+
+export const Default = {
+  args: {
+    user: {
+      id: '1',
+      profile: {
+        name: 'name',
+        displayName: 'displayName',
+      },
+    },
+    numOfPosts: 120,
+  },
+} satisfies Story;
