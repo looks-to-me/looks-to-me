716f3b7d43d4cfab75cbe80e8a70bedfe5b9eab0
sakiyama@penpen <53581722+penpendayo@users.noreply.github.com>
2023-12-28 22:46:18 +0900

    Update apps/web/src/app/_libs/database/schema/tables/post-copy.ts
    Co-authored-by: Karibash <40270352+Karibash@users.noreply.github.com>

--- apps/web/src/app/_libs/database/schema/tables/post-copy.ts
+++ apps/web/src/app/_libs/database/schema/tables/post-copy.ts
@@ -8,17 +8,13 @@ import {
 
 import { posts } from './posts';
 
-export const postCopies = sqliteTable(
-  'post_copies',
-  {
-    postId: text('post_id').notNull().references(() => posts.id),
-    ipAddress: text('ip_address').notNull(),
-    copiedAt: integer('copied_at', { mode: 'timestamp_ms' }).notNull(),
-  },
-  (t) => ({
-    pk: primaryKey({ columns: [t.copiedAt, t.postId, t.ipAddress ] }),
-  }),
-);
+export const postCopies = sqliteTable('post_copies', {
+  postId: text('post_id').notNull().references(() => posts.id),
+  ipAddress: text('ip_address').notNull(),
+  copiedAt: integer('copied_at', { mode: 'timestamp_ms' }).notNull(),
+}, t => ({
+  pk: primaryKey({ columns: [t.copiedAt, t.postId, t.ipAddress] }),
+}));
 
 export const postCopiesRelations = relations(postCopies, ({ one }) => ({
   post: one(posts),
