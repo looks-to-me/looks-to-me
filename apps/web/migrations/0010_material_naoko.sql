b2b61b0c7e5f55d8df30973aad937a5272fe96fe
sakiyama@penpen <53581722+penpendayo@users.noreply.github.com>
2023-12-28 22:40:16 +0900

    Fixed to leave issue comments
    Co-authored-by: Karibash <40270352+Karibash@users.noreply.github.com>

--- apps/web/migrations/0010_material_naoko.sql
+++ apps/web/migrations/0010_material_naoko.sql
@@ -2,6 +2,8 @@ CREATE TABLE `post_copies` (
 	`post_id` text NOT NULL,
 	`ip_address` text NOT NULL,
 	`copied_at` integer NOT NULL,
-	PRIMARY KEY(`copied_at`, `post_id`, `ip_address`),-- NOTE: A bug in the Drizzle ORM prevented the composite primary key from being ordered as intended, so it was rewritten manually
+        -- NOTE: A bug in the Drizzle ORM prevented the composite primary key from being ordered as intended, so it was rewritten manually
+        -- @see: https://github.com/drizzle-team/drizzle-kit-mirror/issues/110
+	PRIMARY KEY(`copied_at`, `post_id`, `ip_address`),
 	FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`) ON UPDATE no action ON DELETE no action
 );
