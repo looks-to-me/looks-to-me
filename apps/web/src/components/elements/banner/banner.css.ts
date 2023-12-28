03bca59e3052408a675244107347a527773d30ff
Karibash <y.karibayashi@gmail.com>
2023-12-28 11:19:13 +0900

    feature: Adjust the padding value

--- apps/web/src/components/elements/banner/banner.css.ts
+++ apps/web/src/components/elements/banner/banner.css.ts
@@ -5,9 +5,9 @@ import { theme } from '../../../themes';
 export const wrapper = style({
   display: 'flex',
   justifyContent:'center',
-  padding: '8px',
-  color: theme.color.token.banner.text,
+  padding: '10px',
   fontWeight: 600,
+  color: theme.color.token.banner.text,
   backgroundColor: theme.color.token.banner.background,
   borderBottom: `solid 1px ${theme.color.token.banner.border}`,
 });
